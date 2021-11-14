dfc5ed656ed09e2d8f90d910050435623143f2f3
Lucian Kallee <luciankallee@gmail.com>
2021-11-14 14:37:22 +0200

    another update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,79 +1,77 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route } from "react-router";
-import { Link, useHistory, Switch } from 'react-router-dom';
+import { Link, useHistory, Switch } from "react-router-dom";
 import Compose from "./Compose";
 import Blogs from "./Blogs";
-import { Button, Alert} from "react-bootstrap";
-import {useAuth} from "../components/Auth"
+import { Button, Alert } from "react-bootstrap";
+import { useAuth } from "../components/Auth";
 import UpdateProfile from "./UpdateProfile";
 
+function Dashboard() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
 
-function Dashboard(){
-const [error, setError ] = useState("");
-const {currentUser, logout} = useAuth();
-const history = useHistory();
+  async function handleLogout() {
+    setError("");
 
-
-
-
-
- async function handleLogout() {
- setError("");
-
- try {
-   await logout();
-   history.push("/login");
- } catch{
-   setError("failed to logout")
- }
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("failed to logout");
+    }
   }
 
-  
-    return (
-        <Router>
-        <div >
-          <nav className="Nav">
-          <a className="brand" href="/">Blogger</a>
-            <ul>
-              <li>
-                <Link className="Nav-item" to="/Blogs">Home</Link>
-              </li>
-              <li>
-                <Link className="Nav-item" to="/Compose">Compose</Link>
-              </li>
-             
-              <li>
-              <strong>Email:</strong> {currentUser.email}
-              </li>
-            </ul>
-          </nav>
-        {error && <Alert variant="danger">{error}</Alert>}
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/Compose">
-              <Compose />
-            </Route>
-            <Route path="/Blogs">
-              <Blogs />
-            </Route>
-            <Route path="/update-profile">
-              <UpdateProfile />
-            </Route>
-          </Switch>
-        </div>
-       
-       
+  return (
+    <Router>
+      <div>
+        <nav className="text-white w-10 md:w-full">
+          <ul className="flex flex-row text-2xl sm:text-4xl">
+            <li className="p-2 sm:p-4">Blogger</li>
+            <li className="p-2 sm:p-4">
+              <Link className="no-underline text-white" to="/Blogs">
+                Home
+              </Link>
+            </li>
+            <li className="p-2 sm:p-4">
+              <Link
+                className="no-underline text-white hover:text-gray-800"
+                to="/Compose"
+              >
+                Compose
+              </Link>
+            </li>
 
-        <div className="logout">
-     <Button onClick={handleLogout} variant="contained" className="btn btn-primary">
-        Logout
-      </Button>
-     </div>
-     
-      </Router> 
-    )
+            <li className="p-2 sm:p-4">{currentUser.email}</li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/Compose">
+            <Compose />
+          </Route>
+          <Route path="/Blogs">
+            <Blogs />
+          </Route>
+          <Route path="/update-profile">
+            <UpdateProfile />
+          </Route>
+        </Switch>
+      </div>
+
+      <div className="logout">
+        <Button
+          onClick={handleLogout}
+          variant="contained"
+          className="btn btn-primary"
+        >
+          Logout
+        </Button>
+      </div>
+    </Router>
+  );
 }
 
 export default Dashboard;
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import { useAuth } from "../components/Auth";
 import { Link, useHistory } from "react-router-dom";
 
-
-
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
+    height: "100vh",
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
     height: "100%",
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -51,95 +51,92 @@ function Register() {
   const { signup } = useAuth();
   const [signIn, setLogin] = useState({
     email: "",
-    password: ""
+    password: "",
   });
- const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-function handleChange(event){
-    const {name, value} = event.target;
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-    setLogin((preValue) =>{
-      return {...preValue, [name]: value};
+    setLogin((preValue) => {
+      return { ...preValue, [name]: value };
     });
   }
 
-  async function handleSubmit(event){
-  event.preventDefault();
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-  try {
-  setLoading(true);
- await signup(signIn.email, signIn.password);
- history.push("/");
-} catch {
-  console.log("Failed to signup!!")
-}
-  setLoading(false);
-
- }
-
-  
+    try {
+      setLoading(true);
+      await signup(signIn.email, signIn.password);
+      history.push("/");
+    } catch {
+      console.log("Failed to signup!!");
+    }
+    setLoading(false);
+  }
 
   return (
     <div>
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Register
-          </Typography>
-          
-          <form  onSubmit={handleSubmit} className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              value={signIn.email}
-              onChange={handleChange}
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              value={signIn.password}
-              onChange={handleChange}
-              id="password"
-              autoComplete="current-password"
-              placeholder="Password must be at least six characters"
-              
-            />
-            
-            <Button
-              type="submit"
-              onClick={handleSubmit}
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              disabled={loading}
-            >
-              Sign In
-            </Button>
-            <p>Already have an account? <Link to="/login">Login</Link></p>
-          </form>
-        </div>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+
+            <form onSubmit={handleSubmit} className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                value={signIn.email}
+                onChange={handleChange}
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                value={signIn.password}
+                onChange={handleChange}
+                id="password"
+                autoComplete="current-password"
+                placeholder="Password must be at least six characters"
+              />
+
+              <Button
+                type="submit"
+                onClick={handleSubmit}
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                disabled={loading}
+              >
+                Sign In
+              </Button>
+              <p>
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </form>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
-    
     </div>
   );
 }
